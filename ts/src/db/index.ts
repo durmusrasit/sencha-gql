077fba1fb4a2c394f64eed4182a0fe4686c41609
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-03-08 22:45:39 +0300

    Add initial version

---
+++ ts/src/db/index.ts
@@ -0,0 +1,8 @@
+import { Theme } from "../types";
+
+const themes: Theme[] = [
+  { themeName: "dark", backgroundColor: "#000", foregroundColor: "#fff" },
+  { themeName: "light", backgroundColor: "#fff", foregroundColor: "#000" },
+];
+
+export { themes };
